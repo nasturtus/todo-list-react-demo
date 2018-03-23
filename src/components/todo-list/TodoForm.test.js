f95d114ee6cb4de9c4982da218c467f1b9d79c56
nasturtus <nasturtus@gmail.com>
2018-03-23 11:41:15 +0800

    Complete click submit verify test

--- src/components/todo-list/TodoForm.test.js
+++ src/components/todo-list/TodoForm.test.js
@@ -3,9 +3,9 @@ import { shallow } from "enzyme";
 import TodoForm from "./TodoForm";
 
 describe("TodoForm", () => {
-  it.skip("should verify submit is called on click", () => {
+  it("should verify submit is called on click", () => {
     const mockHandler = jest.fn();
-    const wrapper = shallow(<TodoForm submitTodo={mockHandler} />);
+    const wrapper = shallow(<TodoForm handleSubmit={mockHandler} />);
     expect(wrapper.find("form")).toHaveLength(1);
     wrapper.find("form").simulate("submit", { preventDefault() {} });
     expect(mockHandler).toBeCalled();
